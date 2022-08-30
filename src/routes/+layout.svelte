1f82096776beb8d1fa34d5d3ad77793d4c294f33
sharathdt <sharath.dt@gmail.com>
2022-08-30 22:02:01 +0530

    add page transition

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { navigating } from "$app/stores";
+  import { fly } from "svelte/transition";
+
+  $: urlChanged = $navigating?.from.pathname === $navigating?.to.pathname;
+</script>
+
+{#key urlChanged}
+  <div in:fly={{ delay: 100, duration: 500, x: -30 }}>
+    <slot />
+  </div>
+{/key}
