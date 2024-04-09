3c97f94c3e5a44b0fbc34e50bf4ebfb2e90c28ab
sharath <sharath.dt@gmail.com>
2024-04-09 22:19:50 +0530

    embed a map

--- src/lib/store.js
+++ src/lib/store.js
@@ -14,6 +14,11 @@ You can share short notes with just a link. No database. No storage!
 | data 1  | data 2   |  data 3 |
 | data 11 | data 12  |  data 13|
 
+- embed maps
+
+
+<iframe style="border:0; display:block; width:100%" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2664.642326877131!2d76.59443713513481!3d12.366147804839871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1712681027838!5m2!1sen!2sin" height="450"  allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
 - check the link generated at the bottom.
 
 ### Features
