4a8fdcf5c0d584f4d894c3ebc2bc61a76afc906d
sharath <sharath.dt@gmail.com>
2025-07-17 09:16:46 +0530

    use auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    prerender: {
-      entries: ["/", "/note", "/note/Q"],
-    },
   },
 };
 
